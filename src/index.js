98a1f26cb8b40fe694d26f194c62310089cbc80d
victor-myha <victormyhaletsky@gmail.com>
2021-05-06 09:30:07 +0300

    del basename from hashrouter

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {Provider} from 'react-redux';
 import store from './redux/redux-store';
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
       <Provider store={store}> 
         <App />
       </Provider>
